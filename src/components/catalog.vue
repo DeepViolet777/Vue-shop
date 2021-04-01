5b1a9cd39966b61b5312b129a79ecb8e0feb1ed3
Anna Malorodova <angervenus6@gmail.com>
2021-04-01 19:18:22 +0300

    Update

---
+++ src/components/catalog.vue
@@ -0,0 +1,61 @@
+<template>
+<div class="catalog">
+    <h2>Catalog</h2>
+    <div class="catalog__list">
+    <CatalogItem
+    v-for="product in PRODUCTS"
+    :key="product.article"
+    v-bind:product_data="product"
+    @sendArticle="showArticle"
+     />
+    </div>
+</div>
+</template>
+
+<script>
+import CatalogItem from './catalog-item'
+import {mapActions, mapGetters} from 'vuex'
+
+export default {
+    name: 'catalog',
+    components: {
+        CatalogItem
+    },
+    props:{
+
+    },
+    data(){
+     return{
+        
+     }
+    },
+    computed:{
+    ...mapGetters([
+    'PRODUCTS'
+    ]),
+    },
+    methods:{
+
+      ...mapActions([
+       'GET_PRODUCTS_API'
+      ]),
+        showArticle(data){
+            console.log(data)
+        }
+    },
+     mounted(){
+       this.GET_PRODUCTS_API()
+     }
+}
+</script>
+
+<style lang="scss">
+.catalog {
+  &__list{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: center;
+}
+}
+</style>
---
+++ src/components/main-page.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="main">
+    {{title}}
+    <Catalog />
+    <Cart />
+</div>
+</template>
+
+<script>
+ import Catalog from './catalog'
+ import Cart from './cart'
+
+ export default {
+     name: 'main-page',
+     components: {
+         Catalog,
+         Cart
+
+     },
+     props: {},
+     data() {
+         return {
+        
+         }
+     },
+     computed: {},
+     methods: {},
+     watch: {}
+ }
+</script>
+
+<style>
+.main {
+    max-width: 900px;
+    margin: 0 auto;
+}
+</style>
